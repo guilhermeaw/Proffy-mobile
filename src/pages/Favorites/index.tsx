f16d814475b7b49033fb12eca16c94baf1f775a1
guilhermeaw <guilherme.w@universo.univates.br>
2020-08-10 22:35:56 -0300

    Created Teacher item component

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -4,11 +4,25 @@ import { View } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
+import TeacherItem from '../../components/TeacherItem';
 
 const Favorites: React.FC = () => {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys favoritos" />
+
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { View } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const TeacherList: React.FC = () => {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
